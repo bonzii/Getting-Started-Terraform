c18c2b3b9245fcf0256f70a2443ae17940815000
learnruby <learnruby@gmail.com>
2023-12-29 04:44:51 +0000

    Added variable, outputs and locals files

--- web_app/main.tf
+++ web_app/main.tf
@@ -23,20 +23,23 @@ data "aws_ssm_parameter" "amzn2_linux" {
 
 # NETWORKING #
 resource "aws_vpc" "app" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block           = var.vpc_app_cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags                 = local.common_tags
 
 }
 
 resource "aws_internet_gateway" "app" {
   vpc_id = aws_vpc.app.id
+  tags   = local.common_tags
 
 }
 
 resource "aws_subnet" "public_subnet1" {
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = var.vpc_public_subnet1_cidr_block
   vpc_id                  = aws_vpc.app.id
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  tags                    = local.common_tags
 }
 
 # ROUTING #
@@ -44,14 +47,17 @@ resource "aws_route_table" "app" {
   vpc_id = aws_vpc.app.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.route_table_cidr_block
     gateway_id = aws_internet_gateway.app.id
   }
+
+  tags = local.common_tags
 }
 
 resource "aws_route_table_association" "app_subnet1" {
   subnet_id      = aws_subnet.public_subnet1.id
   route_table_id = aws_route_table.app.id
+
 }
 
 # SECURITY GROUPS #
@@ -59,6 +65,7 @@ resource "aws_route_table_association" "app_subnet1" {
 resource "aws_security_group" "nginx_sg" {
   name   = "nginx_sg"
   vpc_id = aws_vpc.app.id
+  tags   = local.common_tags
 
   # HTTP access from anywhere
   ingress {
@@ -80,9 +87,13 @@ resource "aws_security_group" "nginx_sg" {
 # INSTANCES #
 resource "aws_instance" "nginx1" {
   ami                    = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.public_subnet1.id
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
+  tags = {
+    Name = "Blanka"
+  }
+
 
   user_data = <<EOF
 #! /bin/bash
---
+++ web_app/variables.tf
@@ -0,0 +1,65 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS regions to deploy to"
+  default     = "us-east-1"
+
+}
+
+
+variable "vpc_public_subnet1_cidr_block" {
+  type        = string
+  description = "CIDR Block for Subnet 1 in VPC"
+  default     = "10.0.0.0/24"
+}
+
+
+variable "route_table_cidr_block" {
+  type        = string
+  description = "CIDR Block for route table"
+  default     = "0.0.0.0/0"
+}
+
+
+variable "vpc_app_cidr_block" {
+  type        = string
+  description = "Base CIDR Block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "Enable DNS hostnames in VPC"
+  default     = true
+}
+
+variable "map_public_ip_on_launch" {
+  type        = bool
+  description = "Map a public IP address for Subnet instances"
+  default     = true
+
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Type for EC2 Instnace"
+  default     = "t2.micro"
+}
+
+variable "company" {
+  type        = string
+  description = "The name of the company"
+  default     = "Globomantics"
+}
+
+variable "project" {
+  type        = string
+  description = "The name of the project"
+}
+
+variable "billing_code" {
+  type        = string
+  description = "Billing code for the project"
+
+}
+
+
